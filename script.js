bfe7488f1420dadc6d268fac0b4c2890f4d99273
Steve De La Rosa <stevedelarosa1036@gmail.com>
2023-10-05 19:32:37 -0400

    created starter code

---
+++ script.js
@@ -0,0 +1,3 @@
+let userInput = prompt(
+    "Type your flavors followed by a comma. Examples - 'Str, Cho, Cho, Van'. Available flavors: Str(strawberry), Cho(chocalate), Van(vanilla), Coff(coffee). "
+);
