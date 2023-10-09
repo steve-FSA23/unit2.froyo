737d08f06b1945e8b6e03c73729809ddb73f2592
Steve De La Rosa <stevedelarosa1036@gmail.com>
2023-10-09 19:33:12 -0400

    completed challenge

--- script.js
+++ script.js
@@ -1,3 +1,20 @@
+// Prompt the user to choose flavors separated by commas.
 let userInput = prompt(
     "Type your flavors followed by a comma. Examples - 'Str, Cho, Cho, Van'. Available flavors: Str(strawberry), Cho(chocalate), Van(vanilla), Coff(coffee). "
 );
+// Split the string of flavors into an array of strings and convert to Lowercase
+const stringArray = userInput.toLocaleLowerCase().split(",");
+
+const orders = {};
+
+stringArray.forEach((flav) => {
+    // Check if flavor exist in the "Orders Object" and increase quantity by +1
+    if (flav in orders) {
+        orders[flav]++;
+    } else {
+        // Add new flavor to "Orders" if it doesn't exist
+        orders[flav] = 1;
+    }
+});
+
+console.log(orders);
